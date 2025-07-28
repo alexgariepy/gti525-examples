55fe4562d0c3883662c07b4217fae2ff3dbf4f07
alexgariepy <aiex@hotmail.ca>
2025-07-28 16:10:13 -0400

    cours 11

---
+++ cours11-security/exercises/myself/CSP/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "script-src 'none'");
+  next();
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+  console.log('Serveur en écoute sur http://localhost:3000')
+})
